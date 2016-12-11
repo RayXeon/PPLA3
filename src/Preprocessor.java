645f1738f2f82a032cd2184d9a02e6f9148b0a0e
RayXeon <ryang@unomaha.edu>
2016-12-11 01:44:55 -0600

    Need final recusively deal with define and include

--- src/Preprocessor.java
+++ src/Preprocessor.java
@@ -7,29 +7,49 @@ import java.io.*;
 import java.lang.reflect.Array;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Hashtable;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 import java.io.FileReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class Preprocessor {
-    private static final String  tempFile= "temp.c";
+    private static final String tempFile= "temp.c";
+    private static final String almostFile = "almost.c";
+    private static final String closeFile = "close.c";
+    private static final String finalFile = "final.c";
+    private static Hashtable myTable = new Hashtable();
     public static void main(String[] args) throws IOException
     {
 
+        removeComment("./src/a_test.c",tempFile);
+        removeDefine(tempFile, almostFile);
+//        System.out.println(myTable.get("A_FILE"));
+        removeInclude(almostFile, closeFile);
+        removeInclude(closeFile,finalFile);
+        removeDefine(finalFile, tempFile);
+        removeInclude(tempFile, closeFile);
+        removeDefine(closeFile, almostFile);
+        removeComment(almostFile,tempFile);
+        removeInclude(tempFile, finalFile);
+        removeDefine(finalFile,tempFile);
+        String str;
+        Enumeration names = myTable.keys();
+        while(names.hasMoreElements()) {
+            str = (String) names.nextElement();
+            System.out.println(str + ": " + myTable.get(str));
+        }
 
-        File outputFile = new File(tempFile);
+
+    }
+
+    public static void removeComment(String inFile, String outFile) throws FileNotFoundException{
+        File outputFile = new File(outFile);
 
         PrintStream output = new PrintStream(outputFile);
-        Pattern pattern = Pattern.compile("[A-Z]+");
-        Hashtable myTable = new Hashtable();
         try{
             File file = new File(
-                    "./src/a_test.c");
+                    inFile);
             Scanner input = new Scanner(file);
 
             boolean seperateLineCommentFlag = false;
@@ -38,44 +58,6 @@ public class Preprocessor {
                 String line = input.nextLine();
                 if(line.length() != 0) {
                     String[] words = line.split(" ");
-
-                    /*Detect Define*/
-                    if( words.length>0 && words[0].equals("#define")){
-                        Matcher matcher = pattern.matcher(words[1]);
-                        if(matcher.find()){
-                            System.out.println(line);
-                            System.out.println("Gocha!");
-                            myTable.put(words[1], words[2]);
-                            output.println(words[1] + " = " + words[2]);
-                            continue;
-                        }
-                        output.println(line);
-                        continue;
-
-                    }
-
-                    /*Detect Include*/
-                    if (words.length>0 && words[0].equals("#include")){
-                        if(words[1].startsWith("\"")){
-
-                            String withoutQuotes_path = words[1].replace("\"", "");
-                            try {
-                                File fr = new File(withoutQuotes_path);
-                                Scanner in = new Scanner(fr);
-                                while(in.hasNext()){
-                                    String lineofnewFile = in.nextLine();
-                                    output.println(lineofnewFile);
-                                }
-                            }
-                            catch (IOException e){
-                                System.out.print("Something wront in get another file.");
-                            }
-                            continue;
-                        }
-                        output.println(line);
-                        continue;
-                    }
-
                     /*Single line comment detection*/
                     if(commentTest3(words))
                         continue;
@@ -96,37 +78,109 @@ public class Preprocessor {
                 }
                 output.println(line);
             }
-
-            File file1 = new File(tempFile);
-            File file2 = new File("almost.c");
-            PrintStream output2 = new PrintStream(file2);
-
-            Scanner in2 = new Scanner (file1);
-            while(in2.hasNext()){
-                String line2 = in2.nextLine();
-                if(line2.length() != 0){
-                    String[] words2 = line2.split(" ");
-                    if (words2.length>0 && words2[0].equals("#include")){
-                        Matcher matcher = pattern.matcher(words2[1]);
-                        if(matcher.find()) {
-                            output2.println(words2[0] + " " + myTable.get(words2[1]));
-                            continue;
-                        }
-                    }
-                    output2.println(line2);
-                    continue;
-                }
-                output2.println(line2);
-
-            }
         }
         catch (java.io.FileNotFoundException ex){
             System.out.println("Something is Wrong!");
         }
-
-        System.out.print("This is what you want:" + myTable.get("A_FILE"));
+        System.out.println("The comments are removed!");
     }
 
+    public static void removeDefine(String inFile, String outFile) throws FileNotFoundException{
+        File outputFile = new File(outFile);
+
+        PrintStream output = new PrintStream(outputFile);
+        Pattern pattern = Pattern.compile("[A-Z]+");
+
+        try{
+            File file = new File(
+                    inFile);
+            Scanner input = new Scanner(file);
+
+            while(input.hasNext())
+            {
+                String line = input.nextLine();
+                if(line.length() != 0) {
+                    String[] words = line.split(" ");
+
+                    /*Detect Define*/
+                    if( words.length>0 && words[0].equals("#define")){
+
+                            Matcher matcher = pattern.matcher(words[1]);
+                            if (matcher.find()) {
+                                myTable.put(words[1], words[2]);
+                                continue;
+                            }
+
+                        output.println(line);
+                        continue;
+                    }
+
+                }
+                output.println(line);
+            }
+        }
+        catch (java.io.FileNotFoundException ex){
+            System.out.println("Something is Wrong in removing define!");
+        }
+
+        System.out.println("The defines are removed!");
+    }
+
+    public static void removeInclude(String inFile, String outFile) throws FileNotFoundException{
+        File outputFile = new File(outFile);
+//
+        PrintStream output = new PrintStream(outputFile);
+        Pattern pattern = Pattern.compile("[A-Z]+");
+
+        try{
+            File file = new File(
+                    inFile);
+            Scanner input = new Scanner(file);
+
+            while(input.hasNext())
+            {
+                String line = input.nextLine();
+                if(line.length() != 0) {
+                    String[] words = line.split(" ");
+
+                    /*Detect Include*/
+                    if (words.length>0 && words[0].equals("#include")){
+                        if(words[1].startsWith("\"")){
+
+                            String withoutQuotes_path = words[1].replace("\"", "");
+                            try {
+                                File fr = new File(withoutQuotes_path);
+                                Scanner in = new Scanner(fr);
+                                while(in.hasNext()){
+                                    String lineofnewFile = in.nextLine();
+                                    output.println(lineofnewFile);
+                                }
+                            }
+                            catch (IOException e){
+                                System.out.print("Something wront in get another file.");
+                            }
+                            continue;
+                        }
+                        Matcher matcher = pattern.matcher(words[1]);
+                        if(matcher.find()) {
+                            output.println(words[0] + " " + myTable.get(words[1]));
+                            continue;
+                        }
+                        output.println(line);
+                        continue;
+                    }
+                }
+                output.println(line);
+            }
+
+        }
+        catch (java.io.FileNotFoundException ex){
+            System.out.println("Something is Wrong in removing include!");
+        }
+
+        System.out.println("The includes are removed!");
+
+    }
 
 
     public static int commentTest1(String[] s){
